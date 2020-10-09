5bd6654132089ce4c9b3ef10aa6434a313a0aa42
ilya-shevtsov <Dayzilya@gmail.com>
2020-10-09 19:35:21 +0300

    Start

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import numpy as np
 import pandas as pd
 from tabulate import tabulate
-df = pd.read_csv("data.csv", delimiter=";")
-print(tabulate(df, headers='keys', tablefmt='psql'))
+
+
+def main():
+    file_name = "data.csv"
+    data_frame = read_data(file_name)
+    analyzed_data = analyze_data(data_frame)
+    visualize(analyzed_data)
+
+
+def read_data(file_name):
+    data_frame = pd.read_csv("file_name.csv", delimiter=";")
+    return data_frame
+
+
+def analyze_data(data_frame):
+    return data_frame
+
+
+def visualize(data_frame):
+    print(tabulate(data_frame, headers='keys', tablefmt='psql'))
 
