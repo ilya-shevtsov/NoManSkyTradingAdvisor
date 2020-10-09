0d1a32fc430b572987323151a9bcc27a9cd57e70
ilya-shevtsov <Dayzilya@gmail.com>
2020-10-09 19:23:31 +0300

    AddreadingCSV

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello world")
+import numpy as np
+import pandas as pd
+from tabulate import tabulate
+df = pd.read_csv("data.csv", delimiter=";")
+print(tabulate(df, headers='keys', tablefmt='psql'))
+
