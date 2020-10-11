c4e54390280a05e55b01bc7410a598a1f149fcc3
ilya-shevtsov <Dayzilya@gmail.com>
2020-10-11 11:32:24 +0300

    Showing data and sorting it by max and min value

--- main.py
+++ main.py
@@ -16,22 +16,19 @@ def read_data(file_name):
 
 
 def analyze_data(data_frame):
-    # data_frame["Highest price"] = ""
-    # data_frame["Lowest price"] = ""
-    # p = data_frame.sort_values(by=["ItemID", "SystemID"])
-    # p = data_frame.max(axis=0)
-    # p = data_frame.groupby(['ItemID']).max()
-    p = data_frame.groupby('ItemID').agg({'Buying': min, 'Selling': max}).reset_index()
-    print(p)
-    return data_frame
+    data_frame = data_frame.replace(0, np.NaN)
+    sorted_data = data_frame.groupby('ItemID').agg({'Buying': min, 'Selling': max}).reset_index()
+    
+    print(sorted_data)
+    return sorted_data
 
 
 def visualize(data_frame):
     data_frame.head()
-    # fig = go.Figure(data=[go.Table(header=dict(values=list(data_frame.columns)),
-    #                                cells=dict(values=[data_frame.ItemID, data_frame.SystemID, data_frame.Buying,
-    #                                                   data_frame.Selling]))])
-    # fig.show()
+    fig = go.Figure(data=[go.Table(header=dict(values=list(data_frame.columns)),
+                                   cells=dict(values=[data_frame.ItemID, data_frame.Buying,
+                                                      data_frame.Selling]))])
+    fig.show()
 
 
 if __name__ == '__main__':
