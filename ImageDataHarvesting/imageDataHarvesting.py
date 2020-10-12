c2d67e4f7f1abc40f1ddca3edda35a778b8c7e82
ilya-shevtsov <Dayzilya@gmail.com>
2020-10-12 16:42:52 +0300

    Item_get_name works

--- ImageDataHarvesting/imageDataHarvesting.py
+++ ImageDataHarvesting/imageDataHarvesting.py
@@ -7,8 +7,11 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Users\dayzi\AppData\Local\Tesseract
 
 def main():
     screenshot_name = "Buying.png"
-    system_name = get_system_name(screenshot_name)
-    print(system_name[:-2])
+    # system_name = get_system_name(screenshot_name)
+    item_name = get_item_name(screenshot_name)
+    print(item_name[:-2])
+    # print(system_name[:-2])
+
 
 def get_system_name(screenshot_name):
     screenshot = Image.open(screenshot_name).convert('LA')
@@ -25,7 +28,19 @@ def get_system_name(screenshot_name):
     return text
 
 
-# def get_item_name(screenshot_name)
+def get_item_name(screenshot_name):
+    screenshot = Image.open(screenshot_name).convert('LA')
+    width = screenshot.width
+    height = screenshot.height
+    left = 1165
+    top = 250
+    right = width - 320
+    bottom = height - 793
+    img_crop = screenshot.crop((left, top, right, bottom))
+    img_crop.save('temp_files/Item_temp.png')
+    img = cv2.imread('temp_files/Item_temp.png')
+    text = pytesseract.image_to_string(img)
+    return text
 
 
 if __name__ == '__main__':
