07c5ba3d42d0569b7a5d81d754c5a61aabaf4ae7
ilya-shevtsov <Dayzilya@gmail.com>
2020-10-12 17:26:35 +0300

    crop working

--- ImageDataHarvesting/imageDataHarvesting.py
+++ ImageDataHarvesting/imageDataHarvesting.py
@@ -7,11 +7,10 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Users\dayzi\AppData\Local\Tesseract
 
 def main():
     screenshot_name = "Buying.png"
-    # system_name = get_system_name(screenshot_name)
-    # item_name = get_item_name(screenshot_name)
+    system_name = get_system_name(screenshot_name)
+    item_name = get_item_name(screenshot_name)
     item_price = get_item_price(screenshot_name)
-    # print(item_name[:-2], system_name[:-2], item_price[:-2])
-    print(item_price[:-2])
+    print(item_name[:-2], system_name[:-2], item_price[:-2])
 
 
 def get_system_name(screenshot_name):
@@ -31,13 +30,7 @@ def get_system_name(screenshot_name):
 
 def get_item_name(screenshot_name):
     screenshot = Image.open(screenshot_name).convert('LA')
-    width = screenshot.width
-    height = screenshot.height
-    left = 1165
-    top = 250
-    right = width - 320
-    bottom = height - 793
-    img_crop = screenshot.crop((left, top, right, bottom))
+    img_crop = crop_image(screenshot, 1165, 250, 320, 793)
     img_crop.save('temp_files/Item_temp.png')
     img = cv2.imread('temp_files/Item_temp.png')
     text = pytesseract.image_to_string(img)
@@ -57,6 +50,15 @@ def get_item_price(screenshot_name):
     img = cv2.imread('temp_files/Item_price_temp.png')
     text = pytesseract.image_to_string(img)
     return text
+
+
+def crop_image(screenshot, left, top, right_offset, bottom_offset):
+    width = screenshot.width
+    height = screenshot.height
+    right = width - right_offset
+    bottom = height - bottom_offset
+    img_crop = screenshot.crop((left, top, right, bottom))
+    return img_crop
 
 
 if __name__ == '__main__':
