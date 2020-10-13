ad034d9d6f887bb5aceb886c7cbf1ba60059f3a7
ilya-shevtsov <Dayzilya@gmail.com>
2020-10-13 15:49:12 +0300

    Twicks

--- ImageDataHarvesting/imageDataHarvesting.py
+++ ImageDataHarvesting/imageDataHarvesting.py
@@ -8,12 +8,11 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Users\dayzi\AppData\Local\Tesseract
 def main():
     screenshot_name = "Buying.png"
     grayscale_screenshot = Image.open(screenshot_name).convert('LA')
-    # system_name = get_system_name(grayscale_screenshot)
-    # item_name = get_item_name(grayscale_screenshot)
+    system_name = get_system_name(grayscale_screenshot)
+    item_name = get_item_name(grayscale_screenshot)
     item_price = get_item_price(grayscale_screenshot)
-    # is_an_item = check_is_item(grayscale_screenshot)
-    # print(item_name, system_name, item_price)
-    print(item_price)
+    is_an_item = check_is_item(grayscale_screenshot)
+    print(item_name, system_name, item_price)
 
 
 # def count_items(screenshot):
@@ -28,8 +27,8 @@ def check_is_item(screenshot):
     # img_crop = crop_image(screenshot, left=1180, top=289, right_offset=670, bottom_offset=770)
     # img_crop = crop_image(screenshot, left=1180, top=388, right_offset=670, bottom_offset=660)
     # img_crop = crop_image(screenshot, left=1180, top=505, right_offset=670, bottom_offset=550)
-    # img_crop = crop_image(screenshot, left=1180, top=615, right_offset=670, bottom_offset=440)
-    # img_crop = crop_image(screenshot, left=1180, top=725, right_offset=670, bottom_offset=330)
+    # img_crop = crop_image(screenshot, left=1180, top=616, right_offset=670, bottom_offset=440)
+    img_crop = crop_image(screenshot, left=1180, top=725, right_offset=670, bottom_offset=330)
     img_crop.save('temp_files/check_is_item_temp.png')
     img = cv2.imread('temp_files/check_is_item_temp.png')
     text = pytesseract.image_to_string(img)
@@ -55,7 +54,7 @@ def get_item_name(screenshot):
         # img_crop = crop_image(screenshot, left=1165, top=365, right_offset=320, bottom_offset=681)
         # img_crop = crop_image(screenshot, left=1165, top=455, right_offset=320, bottom_offset=571)
         # img_crop = crop_image(screenshot, left=1165, top=555, right_offset=320, bottom_offset=459)
-        # img_crop = crop_image(screenshot, left=1165, top=655, right_offset=320, bottom_offset=349)
+        img_crop = crop_image(screenshot, left=1165, top=655, right_offset=320, bottom_offset=349)
         img_crop.save('temp_files/Item_temp.png')
         img = cv2.imread('temp_files/Item_temp.png')
         text = pytesseract.image_to_string(img)
@@ -65,11 +64,12 @@ def get_item_name(screenshot):
 
 def get_item_price(screenshot):
     if check_is_item(screenshot):
+        # bottom in item 4 = 460 or 455 need a check
         # img_crop = crop_image(screenshot, left=1740, top=250, right_offset=-20, bottom_offset=780)
         # img_crop = crop_image(screenshot, left=1741, top=350, right_offset=-20, bottom_offset=680)
-        # img_crop = crop_image(screenshot, left=1741, top=460, right_offset=-20, bottom_offset=570)
-        # img_crop = crop_image(screenshot, left=1741, top=560, right_offset=-20, bottom_offset=460)
-        # img_crop = crop_image(screenshot, left=1741, top=660, right_offset=-20, bottom_offset=350)
+        # img_crop = crop_image(screenshot, left=1741, top=485, right_offset=-20, bottom_offset=570)
+        # img_crop = crop_image(screenshot, left=1741, top=560, right_offset=-20, bottom_offset=455)
+        img_crop = crop_image(screenshot, left=1741, top=660, right_offset=-20, bottom_offset=350)
         img_crop.save('temp_files/Item_price_temp.png')
         img = cv2.imread('temp_files/Item_price_temp.png')
         text = pytesseract.image_to_string(img)
