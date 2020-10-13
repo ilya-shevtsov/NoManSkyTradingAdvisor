a3005e2764761c96c2f2528a8fe9f07ebbdc2558
ilya-shevtsov <Dayzilya@gmail.com>
2020-10-13 13:55:04 +0300

    Works no black spaces and question marks

--- ImageDataHarvesting/imageDataHarvesting.py
+++ ImageDataHarvesting/imageDataHarvesting.py
@@ -11,7 +11,7 @@ def main():
     system_name = get_system_name(grayscale_screenshot)
     item_name = get_item_name(grayscale_screenshot)
     item_price = get_item_price(grayscale_screenshot)
-    print(item_name[:-2], system_name[:-2], item_price)
+    print(item_name, system_name, item_price)
 
 
 # def getting_list_of_item(screenshot):
@@ -21,22 +21,26 @@ def main():
 
 
 def get_system_name(screenshot):
-    img_crop = crop_image(screenshot, left=343, top=940, right_offset=1100, bottom_offset=81)
+    img_crop = crop_image(screenshot, left=310, top=940, right_offset=1100, bottom_offset=81)
     img_crop.save('temp_files/System_temp.png')
     img = cv2.imread('temp_files/System_temp.png')
     text = pytesseract.image_to_string(img)
+    text = text.lstrip("-")
+    text = text.lstrip()
+    text = text[:-2]
     return text
 
 
 def get_item_name(screenshot):
-    # 1 img_crop = crop_image(screenshot, left=1165, top=255, right_offset=320, bottom_offset=791)
-    # 2img_crop = crop_image(screenshot, left=1165, top=355, right_offset=320, bottom_offset=681)
+    img_crop = crop_image(screenshot, left=1165, top=255, right_offset=320, bottom_offset=791)
+    # 2 img_crop = crop_image(screenshot, left=1165, top=355, right_offset=320, bottom_offset=681)
     # 3 img_crop = crop_image(screenshot, left=1165, top=455, right_offset=320, bottom_offset=571)
     # 4 img_crop = crop_image(screenshot, left=1165, top=555, right_offset=320, bottom_offset=459)
     # 5 img_crop = crop_image(screenshot, left=1165, top=655, right_offset=320, bottom_offset=349)
     img_crop.save('temp_files/Item_temp.png')
     img = cv2.imread('temp_files/Item_temp.png')
     text = pytesseract.image_to_string(img)
+    text = text[:-2]
     return text
 
 
@@ -54,7 +58,6 @@ def get_item_price(screenshot):
         img = cv2.imread('temp_files/Item_price_temp.png')
         text = pytesseract.image_to_string(img)
         text = int(text)
-
     return text
 
 
